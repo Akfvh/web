45cec6cdb8dcfc2ea08d1f1fb80df344876f9e93
Akfvh <raradong@naver.com>
2022-08-20 16:54:47 +0900

    comments added

--- whac-a-mole/README.md
+++ whac-a-mole/README.md
@@ -5,12 +5,11 @@
 - 클릭시 점수 +1
 - 타이머 끝날 시 alert, 최종 점수 표시
 <div class="video container">
-    <video width="500" controls="true" autoplay muted>
-        <source src="media/exec_video.mp4" type="video/mp4">
-    </video>
+    <video src="media/exec_video.mp4" style="width:500px" controls="true" loop autoplay muted></video>
 </div>  
 
 <div class="img container">
+    <img src="media/img_exec.png" style="width:500px;">
     <img src="media/img_alert.png" style="width:500px;">
 </div>  
 
--- whac-a-mole/app.js
+++ whac-a-mole/app.js
@@ -1,49 +1,81 @@
-const squares = document.querySelectorAll(".square")
-const mole = document.querySelector(".mole")
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+const squares = document.querySelectorAll(".square")    // get all the squares in the grid
+const timeLeft = document.querySelector('#time-left')   // get time-left displayed
+const score = document.querySelector('#score')          // get score displayed
+const start_button = document.querySelector('.button')  // get start-button
 
-let result = 0
-let hitPosition 
-let currentTime = 60
+let result = 0                  // score computed
+let hitPosition                 // when mole is in the square
+let currentTime = 30             // for timer
+let countDownTimerId = null     // handle for setinterval(countdownTimer)
+let moleTimer = null            // handle for setinterval(moleTimer)
+let prevNum = -1
 
 function randomSquare(){
+    // first, remove '.mole' for every square
     squares.forEach(square => {
         square.classList.remove('mole')
-    }) // for each square, remove the class mole
-
-    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    }) 
+    let randomNum = Math.floor(Math.random() * 9)
+    // set random square '.mole'
+    // prevent repetitive square occurrence
+    while(randomNum === prevNum){
+        randomNum = Math.floor(Math.random() * 9)
+    }
+    let randomSquare = squares[randomNum]
     randomSquare.classList.add('mole')
-    
     hitPosition = randomSquare.id
+    prevNum = randomNum
 }
 
-squares.forEach(square =>{
-    square.addEventListener('mousedown', ()=>{
-        if(square.id == hitPosition){
-            result++
-            score.textContent = result
-            hitPosition = null
+// on clicking START button
+// start moving mole
+// start timer
+start_button.addEventListener('click', ()=>{
+    currentTime = 30;
+    moveMole()
+    startTimer()
+})
+
+squares.forEach(square =>{      
+    square.addEventListener('mousedown', ()=>{      // for every square in the grid,
+                                                    // add eventlister 'mousedown'                
+        if(square.id == hitPosition){               // action:
+            result++                                // if square clicked = square mole
+            score.textContent = result              // result ++ 
+            hitPosition = null                      // initialize square mole
+            square.classList.remove('mole')
         }
     })
 })
-
+/* intervalHandle = setInterval(func, delay) */
 function moveMole(){
-    let timerID = null
-    timerID = setInterval(randomSquare, 1000)
+    moleTimer = setInterval(randomSquare, 800)
 }
-
-
-moveMole()
-
+function startTimer(){
+    countDownTimerId = setInterval(countDown, 1000)
+}
+/* func countDown
+ * update timer
+ * on time == 0,
+ *  - clear interval handles
+ *  - initialize score and grid for next gmae
+ */
 function countDown(){
     currentTime--
     timeLeft.textContent = currentTime
-
+    
     if(currentTime == 0) {
         clearInterval(countDownTimerId)
-        alert('GAME OVER! your final scor is' + result)
+        clearInterval(moleTimer)
+        alert('GAME OVER! your final score is: ' + result)
+        
+        result = 0
+        score.textContent = result
+        
+        squares.forEach(square =>{
+            if(square.classList.contains('mole')){
+                square.classList.remove('mole')
+            }
+        })
     }
 }
-
-let countDownTimerId = setInterval(countDown, 1000)
